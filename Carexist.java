a58e9a823e8088d3a005d5ffa12bf8b0a8343030
Hemapriya14 <99642541+Hemapriya14@users.noreply.github.com>
2022-02-25 22:22:38 +0530

    Add files via upload

---
+++ Carexist.java
@@ -0,0 +1,14 @@
+package week1.day1;
+
+public class Carexist {
+
+	public static void main(String[] args) {
+		
+		car lenovo=new car();
+		  lenovo.applyBreak();
+		  lenovo.applyGear();
+		  lenovo.switchonAc();
+		  lenovo.applyAccelerate();
+	}
+
+}
